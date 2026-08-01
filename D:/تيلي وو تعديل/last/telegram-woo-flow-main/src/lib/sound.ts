bb88e5b7c625ad4f9e6b0e031ec295370eec3ac5
accudesign11-oss <accudesign11@gmail.com>
2026-08-01 13:56:56 -0900

    Initial commit

---
+++ D:/تيلي وو تعديل/last/telegram-woo-flow-main/src/lib/sound.ts
@@ -0,0 +1,8 @@
+// Legacy sound file disabled - Replaced by AudioContext.tsx
+export function playClickSound() {
+  // Disabled legacy audio context oscillator
+}
+
+export function initGlobalClickSound() {
+  // Disabled legacy audio listener
+}
